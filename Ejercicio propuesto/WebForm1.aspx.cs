5291c4cc6b3d5aef86022ee86677223446b0f7dd
Arnold Olanda <olanda188@gmail.com>
2021-10-09 10:44:18 -0500

    Subido codigo de las experiencias de practicas y los ejercicios propuestos

---
+++ Ejercicio propuesto/WebForm1.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Ejercicio_propuesto
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=ARTI\\SQLEXPRESS;Initial Catalog=Ventas;Integrated Security=True";
+            string selectSQL = "SELECT * FROM Productos";
+            //Iniciamos la conexion
+            SqlConnection conexion = new SqlConnection(connectionString);
+            //Ejecutamos la consulta SQL
+            SqlCommand comando = new SqlCommand(selectSQL, conexion);
+            //Almacenamos los datos en un dataAdapter
+            SqlDataAdapter adapter = new SqlDataAdapter(comando);
+            // llenando el dataset
+            DataSet ventas = new DataSet();
+            adapter.Fill(ventas, "clientes");
+            // enlazar el gridview
+            GridView1.DataSource = ventas;
+            GridView1.DataBind();
+        }
+    }
+}
